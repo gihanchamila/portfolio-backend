1dc1b56a9680b80a894de0cde65148312fd1068d
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-03-26 16:49:13 +0530

    connect mongodb cloud

---
+++ init/mongodb.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { connectionUrl } from "../config/kyes.js";
+
+// connect mongodb
+const connectMongodb = async () => {
+    try {
+        if (!connectionUrl) {
+            throw new Error("MongoDB connection URL is undefined! Check environment variables.");
+        }
+
+        await mongoose.connect(connectionUrl, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+
+        console.log("Database connected successfully!");
+    } catch (error) {
+        console.error("MongoDB Connection Error:", error.message);
+        process.exit(1);
+    }
+};
+
+export default connectMongodb
