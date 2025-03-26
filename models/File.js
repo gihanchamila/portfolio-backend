0185f6d1a3c879c826999398a0fcb02dd6927af2
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-03-26 17:46:10 +0530

    Add File model with schema definition

---
+++ models/File.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const fileSchema = mongoose.Schema({
+    key : {type : String, required : true},
+    size : Number,
+    mimeType : String,
+}, {
+    timestamps : true
+});
+
+const File = mongoose.model("file", fileSchema)
+export default File
