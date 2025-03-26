0d72ddbbd2fa33354883e0dd97cb478ac58513ec
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-03-26 16:51:53 +0530

    load env variable and destructured and export them

---
+++ config/keys.js
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
+
+const { PORT, CONNECTION_URL,SENDER_EMAIL, EMAIL_PASSWORD, AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY, AWS_BUCKET_NAME, AWS_REGION, FRONTEND_URL } = process.env;
+
+// Correctly destructure and export the variables
+export const port = PORT;
+export const connectionUrl = CONNECTION_URL;
+export const senderEmail = SENDER_EMAIL;
+export const emailPassword = EMAIL_PASSWORD;
+export const awsAccessKey = AWS_ACCESS_KEY;
+export const awsSecretAccessKey = AWS_SECRET_ACCESS_KEY;
+export const awsBucketName = AWS_BUCKET_NAME;
+export const awsRegion = AWS_REGION
+export const frontendUrl = FRONTEND_URL;
