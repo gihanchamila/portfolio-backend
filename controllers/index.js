6fb864f812cc577017d1e0c95a5fe7c2681b2c7f
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-03-26 18:54:46 +0530

    Add index controller to export individual controllers for file, project, certificate, and resume

---
+++ controllers/index.js
@@ -0,0 +1,6 @@
+import fileController from './fileController';
+import projectController from './projectController';
+import certificateController from './certificate';
+import resumeController from './resume';
+
+export {fileController, projectController, certificateController, resumeController};
