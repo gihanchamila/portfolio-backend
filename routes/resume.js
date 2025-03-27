a688182c6f711fdf508cd97152b2a3b43184ac7f
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-03-27 18:58:16 +0530

    Add ID validation and request validation for resume download route

--- routes/resume.js
+++ routes/resume.js
@@ -1,11 +1,15 @@
 import express from "express";
 import authenticateAPIKey from "../middlewares/authMiddleware.js";
-import {resumeController} from "../controllers/index.js";
+import idValidator from "../validators/idValidator.js"
 import upload from "../middlewares/upload.js";
+
+import {resumeController} from "../controllers/index.js";
+import { validate } from "../validators/validate.js";
+
 
 const router = express.Router();
 
 router.post("/upload", authenticateAPIKey, upload.single("resume"), resumeController.uploadResume);
-router.get("/download/:id", resumeController.downloadResume);
+router.get("/download/:id", idValidator, validate, resumeController.downloadResume);
 
 export default router;
