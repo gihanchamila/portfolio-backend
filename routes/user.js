21c726e879885cfcdb1a9cae6319e4fca165e5dc
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-06-05 12:13:28 +0530

    Remove userEmailValidator from check-verification route and use createUserValidator instead

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { userController  } from "../controllers/index.js";
-import { createUserValidator, userEmailValidator } from "../validators/user.js";
+import { createUserValidator} from "../validators/user.js";
 import { validate }  from "../validators/validate.js";
 
 const router = express.Router();
 
-router.get("/check-verification", userEmailValidator, validate, userController.checkVerification)
+router.get("/check-verification", createUserValidator, validate, userController.checkVerification)
 router.post("/send-verification-code", createUserValidator, validate, userController.sendVerificationCode);
 router.post("/verify-user", createUserValidator, validate,  userController.verifyUser);
 
