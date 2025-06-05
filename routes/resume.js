2dd95dea9555116697a2cd1bdb80339a30396490
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-06-05 12:16:27 +0530

    Replace resume request route validation with createUserValidator for improved user input handling

--- routes/resume.js
+++ routes/resume.js
@@ -2,6 +2,7 @@ import express from "express";
 import authenticateAPIKey from "../middlewares/authMiddleware.js";
 import idValidator from "../validators/idValidator.js"
 import upload from "../middlewares/upload.js";
+import { createUserValidator } from "../validators/user.js";
 
 import {resumeController} from "../controllers/index.js";
 import { validate } from "../validators/validate.js";
@@ -14,7 +15,7 @@ router.get("/get-resumes", isAuth, isAdmin, resumeController.getResumes)
 router.post("/upload", isAuth, isAdmin, upload.single("resume"), resumeController.uploadResume);
 router.get("/download/:id", idValidator, validate, resumeController.downloadResume);
 router.get("/", resumeController.getResumes);
-router.post("/request", resumeController.requestResumeDownload)
+router.post("/request", createUserValidator, validate, resumeController.requestResumeDownload)
 router.delete("/delete/:key", isAuth, isAdmin, resumeController.deleteResume);
 
 export default router;
