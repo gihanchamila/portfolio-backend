19bc2a4215cae61672f6093e612be1a3b965b057
Gihan Chamila <132575646+gihanchamila@users.noreply.github.com>
2025-06-06 16:46:43 +0530

    remove check verification route

--- routes/user.js
+++ routes/user.js
@@ -5,7 +5,6 @@ import { validate }  from "../validators/validate.js";
 
 const router = express.Router();
 
-router.get("/check-verification", createUserValidator, validate, userController.checkVerification)
 router.post("/send-verification-code", createUserValidator, validate, userController.sendVerificationCode);
 router.post("/verify-user", createUserValidator, validate,  userController.verifyUser);
 
